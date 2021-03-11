797c5b3f31556e68fa87ad8b4d270247d39a0203
Nidhi Priya <68498635+nidhipr@users.noreply.github.com>
2021-03-11 21:01:53 +0530

    Create LaunchAppTest.java

---
+++ src/main/java/SeleniumSessions/LaunchAppTest.java
@@ -0,0 +1,17 @@
+package SeleniumSessions;
+
+public class LaunchAppTest {
+
+	public static void main(String[] args) {
+		System.out.println("Remote ----login");
+
+	}
+  
+  public void loginFail(){
+    system.out.println("Remote-------login Fail message");
+	
+	public void logout() {
+		System.out.println("Remote----logout");
+	}
+
+}
