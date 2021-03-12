f6045efe23d14a47622fc2a1ec38e8b9a0cff3c2
Nidhi Priya <68498635+nidhipr@users.noreply.github.com>
2021-03-12 15:31:30 +0530

    Create CloseAppTest.java

---
+++ src/main/java/SeleniumSessions/CloseAppTest.java
@@ -0,0 +1,15 @@
+package SeleniumSessions;
+
+public class CloseAppTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("remote ---logout");
+
+	}
+	public void logout() {
+		System.out.println("remote ---close the browser");
+
+	}
+
+}
