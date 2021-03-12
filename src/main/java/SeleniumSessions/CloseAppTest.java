50360534d6b1979c4b36447de9eee200707706fe
nidhi <nidhi.priya17@gmail.com>
2021-03-12 15:36:57 +0530

    code updated

--- src/main/java/SeleniumSessions/CloseAppTest.java
+++ src/main/java/SeleniumSessions/CloseAppTest.java
@@ -4,12 +4,19 @@ public class CloseAppTest {
 
 	public static void main(String[] args) {
 		
-		System.out.println("remote ---logout");
+     System.out.println("remote ---logout");
 
 	}
 	public void logout() {
 		System.out.println("remote ---close the browser");
 
 	}
+
+	public void quit() {
+	System.out.println("local---quit the browser");
+
+}
+
+
 
 }
